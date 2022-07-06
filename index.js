eea693cd29c3e403dd6997816bf89cd3248d45f0
Travis Davis <travisdavismedia@gmail.com>
2022-07-06 18:33:51 -0500

    commit

--- index.js
+++ index.js
@@ -1,92 +1,45 @@
-function wait(time, error) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            if (error)
-                return reject("FAILED")
-            resolve("DONE")
-        }, time);
+const form = document.getElementById("search");
+const date = document.getElementById('date');
+const weatherInfo = document.getElementById("weather-info");
+const weatherCard = document.getElementById("weather-card");
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const data = new FormData(event.target);
+    data.forEach(function(value, key){
+        console.log(key, value);
     });
-}
-wait(1500).then(text => {
-    console.log(text);
-    return wait(4500);
-}).then(text => {
-    console.log(text)
-    return wait(1500, true);
-}).then(text => {
-    console.log(text);
-}).catch(e => {
-    console.log(e)
-})
-
-async function waitOnThings() {
-    const d1 = await wait(1500);
-    const d2 = await wait(1500);
-    const d3 = await wait(1500);
-    console.log(d1, d2, d3);
-}
-
-waitOnThings();
-const BASE_URL = "https://pokeapi.co/api/v2";
-fetch(`${BASE_URL}/pokemon/ditto`).then((response) => {
-    if (!response.ok)
-        throw "ERROR";
-    return response.json()
-}).then(data => {
-    console.log(data.name)
-})
-
-async function getDitto() {
-    const response = await fetch(`${BASE_URL}/pokemon/ditto`);
-    if (!response.ok)
-        throw "ERROR";
-    const data = await response.json();
-    console.log(data.name);
-}
-getDitto()
-
-async function getPokemon(list = ["ditto", "squirtle", "grotle", "houndour"]) {
-    const response0 = await fetch(`${BASE_URL}/pokemon/${list[0]}`)
-    const response1 = await fetch(`${BASE_URL}/pokemon/${list[1]}`)
-    const response2 = await fetch(`${BASE_URL}/pokemon/${list[2]}`)
-    const response3 = await fetch(`${BASE_URL}/pokemon/${list[3]}`)
-
-    const data0 = await response0.json();
-    const data1 = await response1.json();
-    const data2 = await response2.json();
-    const data3 = await response3.json();
-    console.log(data0, data1, data2, data3);
-}
-getPokemon()
-
-async function getPokemonAtOnce(list = ["ditto", "squirtle", "grotle", "houndour"]) {
-    const responses = await Promise.all([
-        fetch(`${BASE_URL}/pokemon/${list[0]}`),
-        fetch(`${BASE_URL}/pokemon/${list[1]}`),
-        fetch(`${BASE_URL}/pokemon/${list[2]}`),
-        fetch(`${BASE_URL}/pokemon/${list[3]}`),
-    ]);
-    const dataSet = await Promise.all(responses.map(r=>r.json()));
-    console.log(dataSet);
-    dataSet.forEach(p=>{
-        document.body.innerHTML += `<img src="${p.sprites.front_default}"/>`+p.name + "</br>";
-        
-    })
-    //const data0 = await response0.json();
-    //const data1 = await response1.json();
-    //const data2 = await response2.json();
-    //const data3 = await response3.json();
-    //console.log(data0, data1, data2, data3);
-}
-getPokemonAtOnce();
-// Weather API
+    getWeather(data.get("city").trim().toLowerCase(), data.get("state").trim().toLowerCase())
+        .then(createWeatherCard)
+});
 const API_KEY = "e2c2f01a9184c7efcec6757e51818436";
-const API_URL = `http://api.openweathermap.org/data/2.5`
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-async function getForcast(city="memphis"){
-    const response = await fetch(`${API_URL}/weather?q=${city}&appid=${API_KEY}`);
+async function getWeather(city, state, apiKey=API_KEY){
+    const response = await fetch(`${BASE_URL}?appid=${apiKey}&q=${city},${state},US&units=imperial`);
     const data = await response.json();
-    console.log(data);
+    return data;
 }
-getForcast();
-///forecast?q={city name}&appid={API key}`
+
+function createWeatherCard({ weather, main, ...data}){
+    console.log(weather, main, data);
+    const mainText = weather[0].main;
+    const description = weather[0].description;
+    date.innerHTML = `
+        <div class="d-flex flex-column justify-content-center">
+            <p>${mainText}</p>
+            <p>${description}</p>
+        </div>
+    `;
+    weatherInfo.innerHTML = '';
+    for(const key in main){
+        weatherInfo.innerHTML +=  `<li>`;
+        if(key == "temp" && main[key] > 60){
+            weatherInfo.innerHTML += `<i class="bi bi-sun"></i>`
+        }
+        weatherInfo.innerHTML +=  `
+            ${key}: ${main[key]}
+        </li>`
+    }
+    weatherCard.classList.remove("d-none");
+    
+}
